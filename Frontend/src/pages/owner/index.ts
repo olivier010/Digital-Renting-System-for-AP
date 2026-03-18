b2d36a49ca12dac1908e8c3b9e75c6ef0f3f6717
olivier010 <oliviernsengiyumva010@gmail.com>
2026-03-18 12:41:37 +0200

    removed reference for myproperty page

--- Frontend/src/pages/owner/index.ts
+++ Frontend/src/pages/owner/index.ts
@@ -1,3 +1,3 @@
 export { default as OwnerDashboard } from './OwnerDashboard.tsx'
-export { default as MyProperties } from './MyProperties.tsx'
+export { default as Properties } from './Properties.tsx'
 export { default as AddProperty } from './AddProperty.tsx'
