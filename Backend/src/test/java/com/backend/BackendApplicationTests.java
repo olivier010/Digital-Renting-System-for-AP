2b4d8b79eb205f7cf235319b78f0ba9d9516e9a3
YVES Olivier NSENGIYUMVA <oliviernsengiyumva010@gmail.com>
2026-04-12 09:39:34 +0200

    Change test annotation from SpringBootTest to DataJpaTest

--- Backend/src/test/java/com/backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class BackendApplicationTests {
 
     @Test
